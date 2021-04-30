cf2ba0f4bcbca9898cfbabaab2f5c3284eeaf977
Antony George <iamantony88@gmail.com>
2021-04-30 15:15:00 +0530

    Completed CourtCounter

--- app/src/main/java/com/antony/courtcounter/MainActivity.java
+++ app/src/main/java/com/antony/courtcounter/MainActivity.java
@@ -26,68 +26,37 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(scoreTeamA);
     }
 
-    /*
-     * Add 2 points to Team A
-     */
     public void addTwoPointsA(View v){
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
-    /*
-     * Add 1 points to Team A
-     */
     public void addOnePointA(View v){
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
 
-    /*
-     * Add 3 points to Team B
-     */
     public void addThreePointsB(View v){
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
 
-    /*
-     * Add 2 points to Team B
-     */
     public void addTwoPointsB(View v){
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
 
-    /*
-     * Add 1 points to Team B
-     */
     public void addOnePointB(View v){
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
 
 
-    /*
-     * Reset the game
-     */
-    public void reset(View v){
-        scoreTeamA = 0;
-        scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
-    }
-
-    /*
-     * Display Team A Score
-     */
     private void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.score_a);
         scoreView.setText(String.valueOf(score));
     }
 
-    /*
-     * Display Team A Score
-     */
     private void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.score_b);
         scoreView.setText(String.valueOf(score));
